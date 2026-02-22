9ad02b3166ce590585045fec2ae84298aea15204
DongVTHE176699 <Dongvthe176699@fpt.edu.vn>
2026-02-22 17:07:25 +0700

    migration for modifi Appointment

--- Garage_Management.Base/Migrations/20260222100521_modified_Appointment.Designer.cs
+++ Garage_Management.Base/Migrations/20260222100521_modified_Appointment.Designer.cs
@@ -4,6 +4,7 @@ using Garage_Management.Base.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Garage_Management.Base.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260222100521_modified_Appointment")]
+    partial class modified_Appointment
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
