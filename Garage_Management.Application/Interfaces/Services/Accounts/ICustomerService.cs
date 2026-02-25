5e9e1d33fd053b364c86dcd5e6bd25213e7b3cdd
DongVTHE176699 <Dongvthe176699@fpt.edu.vn>
2026-02-25 13:42:28 +0700

    Feat: add list customer and createCustomerBy Receptionist

---
+++ Garage_Management.Application/Interfaces/Services/Accounts/ICustomerService.cs
@@ -0,0 +1,24 @@
+﻿using Garage_Management.Application.DTOs.User;
+using Garage_Management.Base.Common.Models;
+using Garage_Management.Base.Entities.Accounts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage_Management.Application.Interfaces.Services.Accounts
+{
+    public interface ICustomerService
+    {
+        /// <summary>
+        /// Lấy danh sách khách hàng theo phân trang và lọc
+        /// </summary>
+        Task<ApiResponse<PagedResult<CustomerDto>>> GetPagedAsync(ParamQuery query, CancellationToken ct = default);
+
+        /// <summary>
+        /// Receptionist tạo khách hàng mới (tạo User + Customer)
+        /// </summary>
+        Task<ApiResponse<CustomerDto>> CreateCustomerByReceptionistAsync(CreateCustomerRequest request,CancellationToken ct = default);
+    }
+}
