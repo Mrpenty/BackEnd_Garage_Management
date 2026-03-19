bb29a95700a6a6e452ac0a2aaf2071cec088ddcc
Mamazola182 <minhhung23112004@gmail.com>
2026-03-19 15:02:15 +0700

    fix  unittest

--- Garage_Management.UnitTest/JobCards/CreateJobCardTests.cs
+++ Garage_Management.UnitTest/JobCards/CreateJobCardTests.cs
@@ -5,14 +5,10 @@ using Garage_Management.Application.Interfaces.Repositories.JobCards;
 using Garage_Management.Application.Interfaces.Repositories.Services;
 using Garage_Management.Base.Common.Enums;
 using Garage_Management.Base.Entities.Accounts;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Http;
 using Moq;
-using System;
-using System.Threading;
-using System.Threading.Tasks;
 using JobCardEntity = Garage_Management.Base.Entities.JobCards.JobCard;
 using JobCardServiceApp = Garage_Management.Application.Services.JobCards.JobCardService;
-
 namespace Garage_Management.UnitTest.JobCards
 {
     [TestClass]
@@ -25,7 +21,7 @@ namespace Garage_Management.UnitTest.JobCards
         private Mock<IJobCardSparePartRepository> _jobCardSparePartRepo;
         private Mock<IWorkBayRepository> _workBayRepo;
         private Mock<IAppointmentRepository> _appointmentRepo;
-
+        private Mock<IHttpContextAccessor> _httpContextAccessor;
         private JobCardServiceApp _service;
 
         [TestInitialize]
@@ -38,7 +34,7 @@ namespace Garage_Management.UnitTest.JobCards
             _jobCardSparePartRepo = new Mock<IJobCardSparePartRepository>();
             _workBayRepo = new Mock<IWorkBayRepository>();
             _appointmentRepo = new Mock<IAppointmentRepository>();
-
+            _httpContextAccessor = new Mock<IHttpContextAccessor>();
             _service = new JobCardServiceApp(
                 _jobCardRepo.Object,
                 _serviceRepo.Object,
@@ -46,7 +42,8 @@ namespace Garage_Management.UnitTest.JobCards
                 _jobCardServiceRepo.Object,
                 _jobCardSparePartRepo.Object,
                 _workBayRepo.Object,
-                _appointmentRepo.Object
+                _appointmentRepo.Object,
+                 _httpContextAccessor.Object
             );
         }
 
