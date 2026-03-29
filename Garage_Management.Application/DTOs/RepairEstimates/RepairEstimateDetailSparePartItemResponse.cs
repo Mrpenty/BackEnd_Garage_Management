ad21831c3ad077fefde48665f9aa1b72a2f9537c
Mamazola182 <minhhung23112004@gmail.com>
2026-03-29 18:40:08 +0700

    fix

--- Garage_Management.Application/DTOs/RepairEstimates/RepairEstimateDetailSparePartItemResponse.cs
+++ Garage_Management.Application/DTOs/RepairEstimates/RepairEstimateDetailSparePartItemResponse.cs
@@ -3,6 +3,7 @@ namespace Garage_Management.Application.DTOs.RepairEstimates
     public class RepairEstimateDetailSparePartItemResponse
     {
         public int SparePartId { get; set; }
+        public string SparePartName { get; set; } = string.Empty;
         public decimal UnitPrice { get; set; }
         public int Quantity { get; set; }
         public decimal TotalAmount { get; set; }
