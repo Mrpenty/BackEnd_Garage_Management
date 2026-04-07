1a8c8f975e8dcb5dafef2474d931be32b13e7e48
DoVietKhanh2 <dovietkhanh02082003@gmail.com>
2026-04-07 15:05:48 +0700

    feat: add unittest inventory

---
+++ Garage_Management.UnitTest/Inventories/InventoryServiceDeleteTests.cs
@@ -0,0 +1,38 @@
+using Garage_Management.Application.Interfaces.Repositories;
+using Garage_Management.Application.Services.Inventories;
+using Garage_Management.Base.Entities.Inventories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Garage_Management.UnitTest.Inventories
+{
+    [TestClass]
+    public class InventoryServiceDeleteTests
+    {
+        [TestMethod]
+        public async Task DeleteAsync_HasDependencies_Throws()
+        {
+            var repo = new Mock<IInventoryRepository>();
+            var service = new InventoryService(repo.Object);
+            repo.Setup(x => x.GetByIdAsync(1)).ReturnsAsync(new Inventory { SparePartId = 1, PartName = "Bugi" });
+            repo.Setup(x => x.HasDependenciesAsync(1, It.IsAny<CancellationToken>())).ReturnsAsync(true);
+
+            await Assert.ThrowsExceptionAsync<System.InvalidOperationException>(() => service.DeleteAsync(1));
+        }
+
+        [TestMethod]
+        public async Task DeleteAsync_NoDependencies_ReturnsTrue()
+        {
+            var repo = new Mock<IInventoryRepository>();
+            var service = new InventoryService(repo.Object);
+            repo.Setup(x => x.GetByIdAsync(1)).ReturnsAsync(new Inventory { SparePartId = 1, PartName = "Bugi" });
+            repo.Setup(x => x.HasDependenciesAsync(1, It.IsAny<CancellationToken>())).ReturnsAsync(false);
+            repo.Setup(x => x.SaveAsync(It.IsAny<CancellationToken>())).ReturnsAsync(1);
+
+            var result = await service.DeleteAsync(1);
+            Assert.IsTrue(result);
+        }
+    }
+}
---
+++ Garage_Management.UnitTest/Inventories/InventoryServiceGetByBrandTests.cs
@@ -0,0 +1,32 @@
+using Garage_Management.Application.Interfaces.Repositories;
+using Garage_Management.Application.Services.Inventories;
+using Garage_Management.Base.Entities.Inventories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Garage_Management.UnitTest.Inventories
+{
+    [TestClass]
+    public class InventoryServiceGetByBrandTests
+    {
+        [TestMethod]
+        public async Task GetByBrandIdAsync_ReturnsMappedList()
+        {
+            var repo = new Mock<IInventoryRepository>();
+            var service = new InventoryService(repo.Object);
+            repo.Setup(x => x.GetByBrandIdAsync(2, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new List<Inventory>
+                {
+                    new Inventory { SparePartId = 1, PartName = "Bugi", SparePartBrandId = 2 },
+                    new Inventory { SparePartId = 2, PartName = "Lọc gió", SparePartBrandId = 2 }
+                });
+
+            var result = await service.GetByBrandIdAsync(2);
+
+            Assert.AreEqual(2, result.Count);
+        }
+    }
+}
